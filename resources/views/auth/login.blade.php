2e4468d2e577e3ccfde86d3b2ca547a8ca4d20d2
BrianS-TICS <briansanchezaguilar@gmail.com>
2023-01-07 03:44:07 -0700

    add base del proyecto

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,47 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}" novalidate>
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Recordar contraseña') }}</span>
+            </label>
+        </div>
+
+        <div class="flex justify-between my-5">
+            <x-link :href="route('register')">
+                Crear cuenta
+            </x-link>
+            <x-link :href="route('password.request')">
+                Olvidaste tu password
+            </x-link>
+        </div>
+        <x-primary-button class="w-full justify-center">
+            {{ __('Iniciar sesión') }}
+        </x-primary-button>
+    </form>
+</x-guest-layout>
